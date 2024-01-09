306fdb7d09828e05eefc4764e491a8fa2b1de877
fbaker@isd381.org <fbaker@isd381.org>
2024-01-08 22:20:04 -0600

    Intake Subsystem

---
+++ src/main/cpp/subsystems/Intake.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "subsystems/Intake.h"
+#include "Constants.h"
+
+using namespace IntakeConstants;
+
+/*
+Todo: will need to update the type of motor used on the intake once it's known
+Todo: Will need to make methods that make sense for what the intake will do
+- the intake is planned to be a conveyor so will need to turn one direction at a specific speed
+- I'm thinking it should be able to be reversed if we need to 
+-it'd be nice to know if we have a note loaded 
+*/
+Intake::Intake() {
+  // Implementation of subsystem constructor goes here.
+  //frc::PWMSparkMax m_conveyorMotor(kconveyorMotorPort);
+  
+}
+
+frc2::CommandPtr Intake::RunIntake() {
+  // Inline construction of command goes here.
+  // Subsystem::RunOnce implicitly requires `this` subsystem.
+  return RunOnce([ this ] { 
+    m_conveyorMotor.Set(kIntakeSpeed);
+    frc::SmartDashboard::PutString("Intake","Forward"); 
+    });
+  //RunOnce creates a command that calls a lambda once, and then finishes.
+}
+
+frc2::CommandPtr Intake::ReverseIntake() {
+  // Inline construction of command goes here.
+  // Subsystem::RunOnce implicitly requires `this` subsystem.
+  return RunOnce([ this ] { 
+    m_conveyorMotor.Set(-kIntakeSpeed);
+    frc::SmartDashboard::PutString("Intake","Reverse"); 
+    });
+}
+
+frc2::CommandPtr Intake::Stop(){
+return RunOnce([ this ] { 
+  m_conveyorMotor.Set(0);
+    });
+}
+
+bool isLoaded() {
+  // Query some boolean state, such as a digital sensor.
+  return false;
+}
+
+void Intake::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
+void Intake::SimulationPeriodic() {
+  // Implementation of subsystem simulation periodic method goes here.
+}
---
+++ src/main/include/subsystems/Intake.h
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/SubsystemBase.h>
+#include <rev/CANSparkMax.h>
+#include "Constants.h"
+
+class Intake : public frc2::SubsystemBase {
+ public:
+  Intake();
+
+  /**
+   * Example command factory method.
+   */
+  frc2::CommandPtr RunIntake();
+  frc2::CommandPtr ReverseIntake();
+  frc2::CommandPtr Stop();
+  
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a
+   * digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  bool isLoaded();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs during
+   * simulation.
+   */
+  void SimulationPeriodic() override;
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  rev::CANSparkMax m_conveyorMotor{IntakeConstants::kConveyorCanId, rev::CANSparkLowLevel::MotorType::kBrushless};
+  //rev::CANSparkMax m_conveyorMotor(int deviceID, rev::CANSparkLowLevel::MotorType type);
+};
